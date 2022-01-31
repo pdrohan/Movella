61f0ddcab5251d95038a89cf3d4e91cee977098c
pdrohan <peterdrohan38@gmail.com>
2022-01-31 10:08:05 -0600

    final

--- src/goalie_analytics/main.py
+++ src/goalie_analytics/main.py
@@ -1,17 +1,11 @@
 import pandas as pd
 import numpy as np
-import datetime as dt
-import numpy as np
 import webbrowser
 import os 
 import tabulate 
 
 def return_dataframe_(path, show):
-    '''1. This function reads a csv from the specified path location and optionally prints the first 5 rows
-        2. note: openpyxl made this possible, csv was not parse correctly before installing it into the
-        'movellatask virturalenv.
-        3. Use shape to check if the import was successful
-        Note: 'try' added to eliminate empty csv entry
+    '''Error handling read_csv function
     '''
     try:
         goalie_data = pd.read_csv(path)
@@ -22,219 +16,155 @@ def return_dataframe_(path, show):
         print('Note: filename.csv was empty. Please add a file with data')
         exit()
         
-
-
-#Function used within other functions
 def required_columns(dataframe, columns_needed):
-    '''This was created from unit testing findings, ensure the columns required for a given function are
-        in the dataframe provided to said function
-        This code needs to be revisited, the sum / len statement is optimal
+    '''Determines if the dataframe passed has the columns passed
     '''
-    #use this to return the actual dataframe needed
-    # output = dataframe.columns.intersection(columns_needed)
-    #return True if dataframe has teh columns
     output = dataframe.columns.isin(columns_needed)
     if len(columns_needed) == sum(np.array(output, dtype=bool)):
         return True
     else:
         return False
 
-#Function used within other functions
-def get_unique_val_in_col(df, col):
-    '''Returns the number of unique values in the specified column
-        Note: This may be a waste as it was not used as much as anticipated
-    '''
-    return df[col].nunique()
-
 def format_df_columns(dataframe):
-    '''Note 1: This is where I am 
-        Jan 27: Added the required_columns function - need to clarify what is expected from Movella.
+    '''Jan 27: Added the required_columns function - need to clarify what is expected from Mov
+        Jan 31: Not necessary but nice to have 
     '''
     #Check if the columns required for this calculaton exist in the dataframe provided
     if required_columns(dataframe=dataframe, columns_needed=['year', 'tmID']) is False:
         return print('The Dataframe Provided did not have the required columns')
 
     dataframe['year'] = pd.to_datetime(dataframe['year'], format='%Y') 
-    #This changes it from datetime[ns] to a number again - unsure if he wants datatime because it includes
-    #Months and days when that was not provided
-    #not sure if he expects this or not
-    # dataframe['year'] = dataframe['year'].dt.year
     dataframe['tmID'] = dataframe['tmID'].apply(str)
     return dataframe
 
-
-
-def win_or_loss_agg_calc(dataframe, col):
-    '''3. Wins_agg: total wins / total players & 4. Losses_agg: total losses / total players
-    This is the 3rd & 4th task = sum(wins or losses)/countunique(players)
-    need to drop or round to two decimals 
-    updated to call get_unique_val_in_col
-    updated to call required_columns, check if value is too high
+def get_unique_val_in_col(df, col):
+    '''Returns the number of unique values in the specified column
+        Note: This may be a waste as it was not used as much as anticipated
     '''
-    # all_col = col.append('playerID')
-    #Check if the columns required for this calculaton exist in the dataframe provided
-    if required_columns(dataframe=dataframe, columns_needed=[col, 'playerID']) is False:
-        return print('The Dataframe Provided did not have the required columns')
+    return df[col].nunique()
 
-    #Sum the column in question
-    total_wins = dataframe[col].sum()
-    #Call function to find the number of unique players
-    count_of_players = get_unique_val_in_col(dataframe, 'playerID')
-    f_string_name = 'loss Percentage'
-    if col == 'W':
-        f_string_name = 'win Percentage'
-
-    wins_per_plyr = round(total_wins / count_of_players, 2)
-    return f'The average {f_string_name} for all Goalies is {wins_per_plyr}%', wins_per_plyr
-    # return wins_per_plyr
-
-
-
-def agg_all_data(dataframe, col, req):
-    '''Potential improvement: pass a list that is then aggregated instead of dropping columns 
-        and calculated columns for no reason
+def run_analytics(path_to_csv):
+    '''This aggregates all of the desired stats and outputs a Pandas dataframe in HTML
     '''
-    if required_columns(dataframe=dataframe, columns_needed=col) is False:
-        return print('The Dataframe Provided did not have the required columns')
+    #Read in & Format
+    df = return_dataframe_(path_to_csv, False)
+    df2 = format_df_columns(df)
 
-    dataframe = dataframe[col]
-    output = dataframe.agg(req)
-    return output 
-
-
-
-def more_calcs(dataframe):
-    '''5. GP_agg: total games played / total players
-       6. Mins_over_GA_agg: total minutes played / total goals against
-       7. GA_over_SA_agg: total goals against / total shots against
-    '''
-    #Check if the columns required for this calculaton exist in the dataframe provided
-    if required_columns(dataframe=dataframe, columns_needed=['GP', 'playerID', 'Min','GA','SA']) is False:
-        return print('The Dataframe Provided did not have the required columns')
-
-    agg_data = agg_all_data(dataframe=dataframe, col=['GP', 'playerID', 'Min','GA','SA'], req=['sum'])
-    #Games played average
-    sum_of_games_played = agg_data.loc['sum']['GP']
-    count_of_players = get_unique_val_in_col(dataframe, 'playerID')
-    GP_agg = round(sum_of_games_played / count_of_players, 2)
-    #Mins_over_GA_agg: total minutes played / total goals against
-    total_mins_played = agg_data.loc['sum']['Min']
-    total_goals_against = agg_data.loc['sum']['GA']
-    Mins_over_GA_agg = round(total_mins_played / total_goals_against, 2)
-    #GA_over_SA_agg: total goals against / total shots against
-    total_shots_against = agg_data.loc['sum']['SA']
-    GA_over_SA_agg = round(total_goals_against / total_shots_against * 100, 2)
-    # return round(GP_agg, 2), round(Mins_over_GA_agg, 2), round(GA_over_SA_agg, 2)
-    return f'The average games played is: {GP_agg}. The average minutes played per goal against is: {Mins_over_GA_agg} minutes. Goals against over shots against, as a percentage: {GA_over_SA_agg}%',\
-        GP_agg, Mins_over_GA_agg, GA_over_SA_agg
-
-
-def percent_games_won_byplayer(dataframe):
-    '''The output dataframe here should have the same number of rows are the result of get_unique_val_in_col
-    with the same df and col = playerID
-    Note: Currently not a percent, just a decimal with too many decimals.
-    jan 27: This was updated, now returning float for the win % column, 
-            4 columns returned; playerID, W, GP, Win %
-    '''
-    #Check if the columns required for this calculaton exist in the dataframe provided
-    if required_columns(dataframe=dataframe, columns_needed=['playerID','W', 'GP']) is False:
+    if required_columns(dataframe=df2, columns_needed=['tmID','playerID','SA','Min','L','W', 'GP','GA','year']) is False:
         return print('The Dataframe Provided to find the Percentage of Games Won for every Goalie did not have the required columns')
 
-    output = dataframe[['playerID','W', 'GP']]
-    output = output.groupby(['playerID']).sum()
-    output = output[output['W'] != 0]
-    output['Win %'] = round(output.apply(lambda row: row.W / row.GP * 100, axis=1), 2)
-    output.reset_index(inplace=True)
-    #Check if any percentage is greater than 100%
-    if output['Win %'].all() > 100:
-        return print('The dataframe provided may have incorrect data, please review the Win and Games Played columns')
-    return output
+    #Select the columns we need for the analysis
+    output = df[['tmID','playerID','SA','Min','L','W', 'GP','GA','year']]
+    #Group by team ID and year, and aggregate each column with the necessary calculations
+    count_df = output.groupby(['tmID', 'year']).agg(['sum', 'count', 'nunique'])
+    #Reset the index
+    count_df.reset_index(inplace=True)
+    #Declare new dataframe, the following is for team specific aggregations 
+    final_df = pd.DataFrame()
+    #Set the first two columns in new dataframe equal to the count_df columns
+    final_df['tmID'] = count_df['tmID']
+    final_df['year'] = count_df['year']
+    #Calc wins_agg with values from count_df calculations
+    #Total team wins for a specific year divided by the number of goalies they used that year
+    final_df['Wins_agg'] = round(count_df['W']['sum'] / count_df['playerID']['nunique'], 2)
+    #Total team losses for a specific year divided by the number of goalies that played for them that year
+    final_df['Losses_agg'] = round(count_df['L']['sum'] / count_df['playerID']['nunique'], 2)
+    #Total Games played by a team for a specific year divided by the number of goalies that played for them that year
+    final_df['GP_Agg'] = round(count_df['GP']['sum'] / count_df['playerID']['nunique'],2)
+    #The number of minutes the goalies played for a given team for a specific year divided by the number of goals allowed for that team that year 
+    final_df['Mins_over_GA_agg'] = round(count_df['Min']['sum'] / count_df['GA']['sum'],2)
+    #THe number of goals scored for a team over a given year divided by the number of shots against
+    final_df['GA_over_SA_agg'] = round(count_df['GA']['sum'] / count_df['SA']['sum'] * 100,2)
+
+    #This section calculates the player specific aggregations 
+    player_df = pd.DataFrame()
+    #Take the required columns, group by and sum 
+    player_df = df[['tmID','playerID','L','W','GP','year','SA']].groupby(['playerID', 'tmID', 'year']).sum()
+    #Reset index
+    player_df.reset_index(inplace=True)
+    #Add a column with that is a players win percentage for a given team for a given year
+    player_df['Plyr Average Win Percentage'] = round(player_df['W'] / player_df['GP'] * 100, 2)
+    #Join the previous dataframe with the player specific dataframe on the teamID and year 
+    final_df = final_df.merge(player_df, how='left', on=['tmID','year'])
+
+    #New dataframe, We previously calculated player % of games won, now need to find the mean at the 
+    #Team level
+    team_averages_df = player_df.groupby(['tmID', 'year']).mean().round(2)
+    team_averages_df.reset_index(inplace=True)
+    #Merge the new player win % rollup mean to the exisiting output dataframe
+    final_df = final_df.merge(team_averages_df, how='left', on=['tmID','year'])
+    #Rename the columns
+    final_df = final_df.rename(columns={'Plyr Average Win Percentage_x':'Player Win Percentage','Plyr Average Win Percentage_y':'Player Win Percentage mean by Team'})
+
+    #Select the columns that we want to display
+    cols = ['tmID','year','playerID','Wins_agg','Losses_agg','GP_Agg','GA_over_SA_agg', 'Mins_over_GA_agg', 'Player Win Percentage','Player Win Percentage mean by Team']
+    out_df = final_df[cols]
+
+    return out_df
+
+'''Functions called 
+'''
+path_to_csv = 'Goalies.csv'
+out_df = run_analytics(path_to_csv)
+#Send the dataframe to HTML to display more clearly
+out_df.to_html('Aggregated Data 4 Kaggle Hockey Goalie DataSet.html')
+webbrowser.open('file://' + os.path.realpath('Aggregated Data 4 Kaggle Hockey Goalie DataSet.html'))
+df = return_dataframe_(path_to_csv, False)
+df2 = format_df_columns(df)
 
 
-def percent_games_won_byTeam(dataframe):
-    '''percent of games won rate by team
-    '''
-    #Check if the columns required for this calculaton exist in the dataframe provided
-    if required_columns(dataframe=dataframe, columns_needed=['tmID','W', 'GP']) is False:
-        return print('The Dataframe Provided did not have the required columns')
-
-    dff = dataframe[['tmID','W', 'GP']]
-    output = dff.groupby(['tmID']).sum()
-    output['Win Rate %'] = round(output.apply(lambda row: row.W / row.GP * 100, axis=1), 2)
-    output.reset_index(inplace=True)
-    return output
-
-
-
+#This is one of the two functions to output dictionaries
 def most_goals_stopped(dataframe):
     '''Here I am assuming every value in the SA column is considered a goals_stopped
         9. most_goals_stopped: {‘playerID’: playerID, ‘goals_stopped’: goals_stopped}
             • Description: calculate goals stopped per player, then take the player with the max goals
                 stopped and put the details in the dictionary
+        
+        This outputs a dictionary with the player with the most goals stopped
     '''
     #Check if the columns required for this calculaton exist in the dataframe provided
     if required_columns(dataframe=dataframe, columns_needed=['playerID','SA']) is False:
         return print('The Dataframe Provided did not have the required columns')
 
+    #Select required column
     output = dataframe[['playerID','SA']]
+    #Group by player ID, find sums and reset index
     output = output.groupby(['playerID']).sum().reset_index(drop=False)
     # print(output.head())
     max=output['SA'].idxmax()
     max_dict = {'playerID': output.iloc[max][0], 'goals_stopped': output.iloc[max][1]}
     return max_dict
 
-
-
+#Output dictionary function
 def most_efficient(dataframe):
     '''Most efficient is defined as max(SA) / 
     10. most_efficient_player: {‘playerID’: playerID, ‘efficiency’: goals_stopped / minutes played}
         • Description: calculate the goals stopped per minute of play for each player, then take the
         player with the max efficiency just calculated and put the details in the dictionary
+
+        This outputs a dictionary with the player that was the most efficient 
     '''
     #Check if the columns required for this calculaton exist in the dataframe provided
     if required_columns(dataframe=dataframe, columns_needed=['playerID','SA', 'Min']) is False:
         return print('The Dataframe Provided did not have the required columns')
 
+    #Select the required columns 
     dff = dataframe[['playerID','SA', 'Min']]
+    #Sum by playerID and reset the index
     output = dff.groupby(['playerID']).sum().reset_index(drop=False)
+    #Drop columns when SA = 0 because they cant be the most efficient, throws error
     output = output.drop(output[output['SA'] == 0].index)
-    # print(output)
+    # Add a column, witch is Shots Against / Minutes played shown as a percentage
     output['Efficiency'] = round(output.apply(lambda row: row.SA / row.Min * 100, axis=1), 2)
+    #Find the index of the row with the highest efficiency 
     max_efficiency = output['Efficiency'].idxmax()
+    #Output the player ID and efficiency % as requested
     efficiency_dict = {'playerID': output.iloc[max_efficiency][0], 'efficiency': output.iloc[max_efficiency][3]}
     return efficiency_dict
     
 
-
-'''When this file is run, we want all of the appropriate data to be printed, below will call the functions and
-    return the data we want
+'''This is the section that calls the dictionary functions and prints them
 '''
-#This reads in the data 
-path_to_csv = 'Goalies.csv'
-df = return_dataframe_(path_to_csv, False)
-df2 = format_df_columns(df)
-
-print(' ')
-print(' ')
-print('Kaggle Data Goalie Analytics:')
-#This returns the aggregated wins avg
-win_string, w_numbr = win_or_loss_agg_calc(df2, 'W')
-print('->', win_string)
-#This returns the aggregated losses avg
-print(' ')
-loss_string, l_number = win_or_loss_agg_calc(df2, 'L')
-print('->', loss_string)
-print(' ')
-# agg_data = agg_all_data(dataframe=df2, col=['GP'], req=['sum', 'min'])
-#This calculates 5. GP_agg, Mins_over_GA_agg, GA_over_SA_agg
-more_calcs_f_string, GP_agg, Mins_over_GA_agg, GA_over_SA_agg = more_calcs(df2)
-print('->', more_calcs_f_string)
-print(' ')
-#This calculates the % of games won, grouped by goalie
-print(percent_games_won_byplayer(df2).head().to_markdown())
-print(' ')
-#This calculates the % of games won, by team
-print(percent_games_won_byTeam(df2).head().to_markdown())
 print(' ')
 #This returns a dictionary of the Goalie that stopped the most goals 
 print('The Most Goals Stopped by any Goalie:')
@@ -245,31 +175,3 @@ print(' ')
 print('The Most Efficient Goalie: ')
 most_efficient_dict = most_efficient(df2)
 print('->', most_efficient_dict)
-
-# Percent_Games_won_by_team = percent_games_won_byTeam(df2).to_html('Percent_Games_won_by_team.html')
-# percent_games_won_by_plyr = percent_games_won(df2).to_html('Percent_Games_won_by_Player.html')
-# # webbrowser.open('file://' + os.path.realpath('Percent_Games_won_by_team.html'))
-# webbrowser.open('file://' + os.path.realpath('Percent_Games_won_by_Player.html'))
-
-
-'''
-Assessment Task:
-Program and display these aggregates as output
-1. tmID: string
-2. year: Year
-3. Wins_agg: total wins / total players
-4. Losses_agg: total losses / total players
-5. GP_agg: total games played / total players
-6. Mins_over_GA_agg: total minutes played / total goals against
-7. GA_over_SA_agg: total goals against / total shots against
-8. avg_percentage_wins: calculate the percentage of games won for each player, then take the
-mean at team level
-9. most_goals_stopped: {‘playerID’: playerID, ‘goals_stopped’: goals_stopped}
-• Description: calculate goals stopped per player, then take the player with the max goals
-stopped and put the details in the dictionary
-10. most_efficient_player: {‘playerID’: playerID, ‘efficiency’: goals_stopped / minutes played}
-• Description: calculate the goals stopped per minute of play for each player, then take the
-player with the max efficiency just calculated and put the details in the dictionary
-'''
-
-
--- tests/test_goalies.py
+++ tests/test_goalies.py
@@ -1,16 +1,16 @@
 from ctypes import util
-from goalie_analytics.main import return_dataframe_, format_df_columns, required_columns,\
-     percent_games_won_byplayer, percent_games_won_byTeam, w_numbr, l_number, GP_agg,\
-          Mins_over_GA_agg, GA_over_SA_agg, agg_all_data, df2, get_unique_val_in_col,\
-               most_efficient_dict, most_stopped_dict,path_to_csv
+from goalie_analytics.main import return_dataframe_, format_df_columns, df2,\
+               most_efficient_dict, most_stopped_dict,path_to_csv, out_df, run_analytics
 import pytest
 import pandas as pd
-from pandas.util.testing import assert_frame_equal # <-- for testing dataframes
+from pandas.testing import assert_frame_equal, assert_index_equal
+import datatest as dt 
+from datatest import validate
+
 
 '''Welcome to the test file, I have tests for every function and I even made some fake csvs, to create
-    tests for unique csv files. A few tests will fail, because I purposely imported
-    some csvs designed to fail. If you want it to run, with or without errors, comment in or out 
-    lines 15, 17, 18, 20, 21. Line 22 
+    tests for unique csv files. 
+    If you want some tests to fail, un-comment the code on lines 17, 19, 20, 22, 23
 '''
 
 @pytest.mark.parametrize('test_input', [
@@ -26,29 +26,66 @@ from pandas.util.testing import assert_frame_equal # <-- for testing dataframes
 
 class TestParametrized:
 
+    @pytest.mark.mandatory
+    def test_df_correct_size(self, test_input):
+        '''This is the use of assert_dataframe_equals as requested 
+           Checks if the return_dataframe_ function read in the data as expected
+        '''
+        df = return_dataframe_(test_input, False)
+        csv_df = pd.read_csv(test_input)
+        assert_frame_equal(csv_df, df, check_dtype=True)
+
+    @pytest.mark.mandatory
+    def test_columns(self, test_input):
+        '''Ensures the csvs dataframe columns match the required columns 
+        '''
+        df = return_dataframe_(test_input, False)
+
+        dt.validate(
+            df.columns,{'playerID','year','stint','tmID','lgID','GP','Min',	'W','L','T/OL','ENG',\
+                'SHO','GA','SA','PostGP','PostMin','PostW','PostL','PostT','PostENG','PostSHO',\
+                    'PostGA','PostSA'}
+        )
+    
+    @pytest.mark.mandatory
+    def test_output_df(self, test_input):
+        '''Check if the outputted value from run_analytics has the right columns 
+        '''
+        out_cols = run_analytics(test_input).columns
+        requirement = {'tmID','year','playerID','Wins_agg','Losses_agg','GP_Agg','GA_over_SA_agg', 'Mins_over_GA_agg', 'Player Win Percentage','Player Win Percentage mean by Team'}
+        dt.validate(out_cols, requirement)
+
+    def test_check_indexes(self, test_input):
+        '''The indexes in the input csv and the output csv should be the same,
+            when accounting for the duplicate values
+        '''
+        out_df_i = run_analytics(test_input).index
+        # print(out_df)
+        in_df = return_dataframe_(test_input, False)
+        #If there is duplicate values, grouped by 'tmID','playerID','year' they will be aggregated
+        #in the output dataframe from run_analytics, therefore drop them to find the correct index
+        no_duplicates = in_df.drop_duplicates(subset=['tmID','playerID','year'])
+        # print(in_df)
+        assert_index_equal(out_df_i, no_duplicates.index, exact='equiv', check_exact=False, rtol=3)
+
+    @pytest.mark.mandatory
     def test_df_is_none(self, test_input):
         '''Check if the dataframe returned from the given function is None
             if this test passes, the dataframe has data
             True and false make no difference     
         '''
         # df = return_dataframe_(test_input, False)
-        df = return_dataframe_(test_input, True)
+        df = return_dataframe_(test_input, False)
         assert df is not None
 
-    def test_df_has_columns(self, test_input):
-        '''Check if the required columns exist in the dataframe provided by return_dataframe_
-            This by default checks if the dataframe is the right shape (column wise)
-        '''
-        df = return_dataframe_(test_input, False)
-        check_value = required_columns(df, ['tmID','playerID','SA','Min','L','W', 'GP','GA','year'])
-        assert check_value is not False
-    
+    @pytest.mark.mandatory
     def test_def_has_rows(self, test_input):
         '''Check if the dataframe imported has rows, and not just columns
         '''
         df = return_dataframe_(test_input, False)
         assert df.empty is False
-
+    
+    @pytest.mark.mandatory
     def test_column_data_types(self, test_input):
         '''Ensures the datatypes returned from the format_df_columns are correctly specified
             NOTE: this could change if the dtype is not 'Year' - need to check w/ mov.
@@ -57,26 +94,7 @@ class TestParametrized:
         df2 = format_df_columns(df)
         assert df2.dtypes['tmID'] == 'object'
         assert df2.dtypes['year'] == 'datetime64[ns]'
-
-
-@pytest.mark.parametrize('number_in, lower, upper', [
-    (w_numbr, 0, 100),
-    (l_number, 0, 100),
-    (GP_agg, 0, agg_all_data(df2, ['GP'],['sum']).loc['sum']['GP']),
-    (Mins_over_GA_agg, 0, 60),
-    (GA_over_SA_agg, 0, 100), 
-    (most_efficient_dict['efficiency'], 0, 100), 
-    (most_stopped_dict['goals_stopped'], 0, 50000)
     
-])
-
-def test_output_number_bounds(number_in, lower, upper):
-    '''simply checks the calculated value is within reasonable bounds
-        This test assumes that this code will no longer be in production by the time someone reaches 
-        50 000 shots against in the NHL. Current record is 31k.
-    '''
-    assert lower < number_in < upper
-
 
 @pytest.mark.parametrize('dictn, key', [
     (most_efficient_dict, 'efficiency'), 
@@ -84,42 +102,8 @@ def test_output_number_bounds(number_in, lower, upper):
 
 ])
 
+@pytest.mark.mandatory
 def test_dictionary_outputs(dictn, key):
-    assert dictn[key] is not None
-
-
-
-@pytest.mark.parametrize('dataframe_in, expected_rows, col', [
-    (df2, get_unique_val_in_col(df2, 'playerID'), 'playerID'),
-    (df2, get_unique_val_in_col(df2, 'tmID'), 'tmID')
-])
-
-def test_output_dataframe(dataframe_in, expected_rows, col):
-    '''The length of a column can not be longer than the number of unique rows, because these 
-        two functinons being tested group by the specified column.
+    '''Asserts the dictionary functions are outputting values 
     '''
-    if col == 'playerID':
-        df_len = len(percent_games_won_byplayer(dataframe_in))
-    if col == 'tmID':
-        df_len = len(percent_games_won_byTeam(dataframe_in))
-    assert df_len <= expected_rows
-
-
-# print(df2.dtypes)
-# @pytest.mark.parametrize('col, expected', [
-#     ('tmID', df2.dtypes['tmID']),
-#     ('playerID', object),
-#     ('SA', 'float64'),
-#     ('Min','float64'),
-#     ('L','float64'),
-#     ('W','float64'),
-#     ('GP','float64'),
-#     ('GA','float64'),
-#     ('year','datetime64[ns]')
-# ] )
-
-# def test_column_data_types(col, expected):
-#     '''NOTE: ensures the rows required are all there, 
-#         i think i am on the right track but this nee
-#     '''
-#     assert df2.dtypes[col] is expected
+    assert dictn[key] is not None
